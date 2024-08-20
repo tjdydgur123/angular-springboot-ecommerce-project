7d5d3dc4cee16724db0be16556c49ed44df8dab7
YongHyuk Seo <tjdydgur123@gmail.com>
2024-08-19 20:41:26 -0700

    Section 12: search for Products by Category

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -9,11 +9,23 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { provideHttpClient } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { RouterModule, Routes } from '@angular/router';
+
+// The order of the routes is important, first match wins. Starting from most specific to generic
+const routes: Routes = [
+  // When path matches, it will create new instance of given component
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent }, // default category id will be set
+  { path: 'products', component: ProductListComponent }, // default category id will be set
+  { path: '', redirectTo: '/products', pathMatch: 'full' }, // if not given then redirect to the /products
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }, // it will match on anything that didn't match above routes
+];
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
   // HttpClientModule is deprecated... use provideHttpClient instead to inject the HttpClient service
-  imports: [BrowserModule, AppRoutingModule],
+  // Configure the router by adding RouterModule with the routes defined from above
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   // By adding ProductService, we can inject the given service into other class and component
   providers: [provideClientHydration(), provideHttpClient(), ProductService],
   bootstrap: [AppComponent],
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../common/product';
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
@@ -5,23 +6,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  constructor(private productService: ProductService) {}
+  currentCategoryId: number = 1;
+  constructor(
+    private productService: ProductService,
+    // current active route that loaded the component. Need for accessing route parameters
+    private route: ActivatedRoute
+  ) {}
 
   // ngOnInit is called once the ProductListComponent is initialized.
   ngOnInit(): void {
-    this.listProducts();
+    // Whenever the route parameters change, the function inside the subscribe block is executed.
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // then get the "id" param string. convert string to a number using the "+" symbol
+      // "!" is the non-null assertion operator. It tells compiler the object is not null.
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // no category id is available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
     // method is invoked once it is "subscribe"
-    this.productService.getProductList().subscribe((data) => {
-      // assinging received data (from backend api) to the Product array
-      this.products = data;
-    });
+    // get the products for the given catefory id by passing the category id
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        // assinging received data (from backend api) to the Product array
+        this.products = data;
+        // console.log(data);
+      });
   }
 }
