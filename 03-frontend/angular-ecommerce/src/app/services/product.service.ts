165d6575e50dff62e083790eb0158d6b55363ae1
YongHyuk Seo <tjdydgur123@gmail.com>
2024-09-09 08:14:29 -0400

    Section 15: Prodcut master view-detail view

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -5,23 +5,30 @@ import { Product } from '../common/product';
 import { ProductCategory } from '../common/product-category';
 
 // @Injectable allows this given service can be injected into other classes or components
-// providedIn: 'root' -> means allow everywhere
+// providedIn: 'root' -> means allow everywhere or globally
 @Injectable({
   providedIn: 'root',
 })
 
-// This is a Angular service that interacts with a backend API to retrieve data.
+// This is a Angular service that interacts with a backend API for CRUD operations.
 export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
+  // inject the httpClient using the dependency injection
   constructor(private httpClient: HttpClient) {}
+
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
 
   // This method is responsible for making an HTTP GET request to the backend to fetch a list of products.
   // It returns observable. Map the JSON data from Spring Data REST to Product array
   getProductList(theCategoryId: number): Observable<Product[]> {
     // need to build URL based on category id
-    // below endpoint is exposed by Spring Data REST based on the query name
+    // below endpoint is exposed by Spring Data REST based on the query method name
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.getProducts(searchUrl);
@@ -41,8 +48,6 @@ export class ProductService {
 
   private getProducts(searchUrl: string) {
     // send the GET request to the given backend baseUrl or other url using httpClient
-    // The generic type <GetResponseProducts> specifies the expected structure of the response data,
-    // which is defined by the GetResponseProducts interface.
     return (
       this.httpClient
         .get<GetResponseProducts>(searchUrl)
